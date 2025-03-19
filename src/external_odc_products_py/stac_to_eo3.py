8b372949a6470163b11518c43ebfe10af5739b46
Victoria Neema <victoria@kartoza.com>
2025-03-19 18:14:41 +0300

    Update stac to eo3

--- src/external_odc_products_py/stac_to_eo3.py
+++ src/external_odc_products_py/stac_to_eo3.py
@@ -1,25 +1,118 @@
 """
-STAC to EO3 translation
+Tools for STAC to EO3 translation
 Adapted from
 https://github.com/opendatacube/odc-tools/blob/develop/apps/dc_tools/odc/apps/dc_tools/_stac.py
 
-See https://github.com/opendatacube/odc-tools/issues/615
+Once the following issues are resolved the odc-tools s3-to-dc can be used instead:
+https://github.com/opendatacube/odc-tools/issues/622
+https://github.com/opendatacube/odc-tools/issues/615
 """
 
-from typing import Any, Dict
+from pathlib import Path
+from typing import Any, Dict, Optional, Tuple
 
 from odc.apps.dc_tools._docs import odc_uuid
 from odc.apps.dc_tools._stac import (
+    DEA_LANDSAT_PRODUCTS,
+    TO_BE_HARD_CODED_COLLECTION,
     _check_valid_uuid,
+    _find_self_href,
     _geographic_to_projected,
+    _get_region_code,
     _get_stac_bands,
     _get_stac_properties_lineage,
-    _stac_product_lookup,
+    _get_usgs_product_name,
 )
 from odc.geo.geom import Geometry, box
+from pyproj import CRS
 from toolz import get_in
 
 Document = Dict[str, Any]
+
+
+def _stac_product_lookup(
+    item: Document,
+) -> Tuple[str, Optional[str], str, Optional[str], str]:
+    properties = item["properties"]
+
+    dataset_id: str = item["id"]
+    dataset_label = item.get("title")
+
+    # Get the simplest version of a product name
+    product_name = properties.get("odc:product", None)
+
+    # Try to get a known region_code
+    region_code = _get_region_code(properties)
+
+    default_grid = None
+
+    # Maybe this should be the default product_name
+    constellation = properties.get("constellation") or properties.get("eo:constellation")
+    if constellation is not None:
+        constellation = constellation.lower().replace(" ", "-")
+
+    collection = item.get("collection")
+    # Special case for USGS Landsat Collection 2
+    if collection is not None and collection == "landsat-c2l2-sr":
+        product_name = _get_usgs_product_name(properties)
+
+    # It is an ugly hack without interrupting DEAfric sentinel 2
+    if constellation is not None and product_name is None:
+        if constellation == "sentinel-2" and collection in TO_BE_HARD_CODED_COLLECTION:
+            dataset_id = properties.get("sentinel:product_id") or properties.get(
+                "s2:granule_id", dataset_id
+            )
+            if collection == "s2_l2a_c1":
+                product_name = "s2_l2a_c1"
+            else:
+                product_name = "s2_l2a"
+            if region_code is None:
+                try:
+                    # Support v1.0.0 to v1.2.0 of the projection STAC extension
+                    # expected format "proj:epsg": 3857
+                    native_crs = properties["proj:epsg"]
+                    native_crs = CRS.from_epsg(native_crs).to_string()
+                except KeyError:
+                    # Support v2.0.0 of the projection STAC extension
+                    # expected format "proj:code": "EPSG:3857"
+                    native_crs = properties["proj:code"]
+
+                # Let's try two options, and throw an exception if we still don't get it
+                try:
+                    # The 'mgrs' prefix (and STAC extension) started with STAC v1.0.0
+                    region_code = (
+                        f"{CRS(native_crs).to_authority()[1][-2:]}"
+                        f"{properties['mgrs:latitude_band']}"
+                        f"{properties['mgrs:grid_square']}"
+                    )
+                except KeyError:
+                    region_code = (
+                        f"{CRS(native_crs).to_authority()[1][-2:]}"
+                        f"{properties['sentinel:latitude_band']}"
+                        f"{properties['sentinel:grid_square']}"
+                    )
+
+            default_grid = "g10m"
+
+    # If we still don't have a product name, use collection
+    if product_name is None:
+        product_name = collection
+        if product_name is None:
+            raise ValueError("Can't find product name from odc:product or collection.")
+
+    # Product names can't have dashes in them
+    product_name = product_name.replace("-", "_")
+
+    if product_name in DEA_LANDSAT_PRODUCTS:
+        self_href = _find_self_href(item)
+        dataset_label = Path(self_href).stem.replace(".stac-item", "")
+        default_grid = "g30m"
+
+    # If the ID is not cold and numerical, assume it can serve a label.
+    if not dataset_label and not _check_valid_uuid(dataset_id) and not dataset_id.isnumeric():
+        dataset_label = dataset_id
+
+    return dataset_id, dataset_label, product_name, region_code, default_grid
 
 
 def stac_transform(input_stac: Document) -> Document:
@@ -70,19 +163,19 @@ def stac_transform(input_stac: Document) -> Document:
 
     stac_properties, lineage = _get_stac_properties_lineage(input_stac)
 
-    # Support https://stac-extensions.github.io/projection/v2.0.0/schema.json
     try:
-        # Expected format "proj:code": "EPSG:3857"
-        epsg = properties["proj:code"]
-        native_crs = properties["proj:code"].lower()
+        # Support v1.0.0 to v1.2.0 of the projection STAC extension
+        # expected format "proj:epsg": 3857
+        native_crs = properties["proj:epsg"]
+        native_crs = CRS.from_epsg(native_crs).to_string()
     except KeyError:
-        # v1.0.0 to v1.2.0 expected format "proj:epsg": 3857
-        epsg = properties["proj:epsg"]
-        native_crs = f"epsg:{epsg}"
+        # Support v2.0.0 of the projection STAC extension
+        # expected format "proj:code": "EPSG:3857"
+        native_crs = properties["proj:code"]
 
     # Transform geometry to the native CRS at an appropriate precision
-    geometry = Geometry(input_stac["geometry"], "epsg:4326")
-    if native_crs != "epsg:4326":
+    geometry = Geometry(input_stac["geometry"], "EPSG:4326")
+    if native_crs != "EPSG:4326":
         # Arbitrary precisions, but should be fine
         pixel_size = get_in(["default", "transform", 0], grids, no_default=True)
         precision = 0
